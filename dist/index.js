b28576491e390c22b6245d336ba538c194d85326
Juma Antony <jumaantony840@gmail.com>
2023-07-06 11:16:48 +0300

    TypeScript Datatypes

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,47 @@
 "use strict";
-let id = 5;
-console.log('ID:', id);
+// TypeScript Basic Types
+let id = 5; // number type
+let company = 'Juma Antony'; // string type
+let isPublished = true; // bollean type
+let x = 'Hello'; // any type  - can be any type
+let ids = [1, 2, 3, 4, 5]; // array of numbers
+// ids.push('hello'); this will throw an error
+let arr = [1, true, 'hello']; // arrays of different types
+// tuple
+let person = [1, 'Juma', true]; // tuple
+// tuple array
+let employee; // array of tuples
+employee = [
+    [1, 'Juma'],
+    [2, 'Antony'],
+    [3, 'Juma Antony'],
+];
+// union
+let pid; // can be either string or number
+pid = '22';
+// enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Up); // 0
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Up); // Up
+const user = {
+    id: 1,
+    name: 'Juma',
+};
+console.log(user);
+// type assertion tells the compiler that we want to treat an entity as a different type
+let cid = 1;
+// let customerId = <number>cid;
+let customerId = cid;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,69 @@
-let id = 5
+// TypeScript Basic Types
 
-console.log('ID:', id)
+let id: number = 5; // number type
+let company: string = 'Juma Antony'; // string type
+let isPublished: boolean = true; // bollean type
+let x: any = 'Hello'; // any type  - can be any type
+
+let ids: number[] = [1, 2, 3, 4, 5]; // array of numbers
+
+// ids.push('hello'); this will throw an error
+
+let arr: any[] = [1, true, 'hello']; // arrays of different types
+
+// tuple
+let person: [number, string, boolean] = [1, 'Juma', true]; // tuple
+
+// tuple array
+let employee: [number, string][]; // array of tuples
+
+employee = [
+    [1, 'Juma'],
+    [2, 'Antony'],
+    [3, 'Juma Antony'],
+];
+
+// union
+
+let pid: string | number; // can be either string or number
+
+pid = '22';
+
+// enum
+
+enum Direction1 {
+    Up, // by default it is zero
+    Down,
+    Left,
+    Right,
+}
+
+console.log(Direction1.Up); // 0
+
+enum Direction2 {
+    Up = 'Up',
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right',
+}
+
+console.log(Direction2.Up); // Up
+
+// objects
+type User = {
+    id: number;
+    name: string;
+};
+const user: User = {
+    id: 1,
+    name: 'Juma',
+};
+
+console.log(user);
+
+// type assertion tells the compiler that we want to treat an entity as a different type
+let cid: any = 1;
+
+// let customerId = <number>cid;
+
+let customerId = cid as number;
